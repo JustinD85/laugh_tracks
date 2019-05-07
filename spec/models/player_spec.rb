025e0ca4d90a12998b7914586102a4c578a9b6c4
Justin Duncan <falsemotive@icloud.com>
2019-05-07 07:01:29 -0600

    Add average age test

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -14,24 +14,22 @@ describe Player, type: :model do
   end
 
   describe "class methods" do
-  attr_reader :player_1,:player_2
+    attr_reader :player_1,:player_2
 
     before :each do
+      @player_1 = create(:player) 
+      @player_2 = create(:player) 
 
-   @player_1 = create(:player) 
-   @player_2 = create(:player) 
-
-    player_1.cards << create(:card)
+      player_1.cards << create(:card)
     end
 
     it "should return players whose age match show_by_age arguement" do
      
-    expect(Player.select_by_age(player_1.age)).to  eq([player_1])
-
+      expect(Player.select_by_age(player_1.age)).to  eq([player_1])
     end
 
     it "should return avg age" do
-      
+      expect(Player.avg_age).to eq(Player.average(:age))
     end
   end
 end
